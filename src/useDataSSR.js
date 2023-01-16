af2a575cbc767dfad710279d6ab42de0aba5f8c4
Donte Small <donte.small@zoom.us>
2023-01-16 10:59:55 -0500

    Load Data from Client-side if not loaded from server side using context

--- src/useDataSSR.js
+++ src/useDataSSR.js
@@ -3,18 +3,20 @@ import { InitialDataContext } from "./InitialDataContext";
 
 export const useDataSSR = (resourceName, loadFunc) => {
   const context = useContext(InitialDataContext);
-  const [data, setData] = useState(context._data[resourceName]);
+	const [data, setData] = useState(context._data[resourceName]);
 
-  // check if data is loaded from the server side
-  if (context.isServerSide) {
+  
+  if (context._isServerSide) {
+    console.log(" Data is loaded from the server side");
     context._requests.push(
-      loadFunc().then((result) => (context._data[resourceName] = result))
+      loadFunc().then( result => (context._data[resourceName] = result) )
     );
-  } else if (!data) {
-    // if data is not loaded from the server side, load it from the client side
-    loadFunc().then((result) => {
-      setData(result);
 
+  } else if (!data) {
+    
+    console.log(" Data is loaded from the client side");
+    loadFunc().then(result => {
+      setData(result);
       context._data[resourceName] = result;
     });
   }
